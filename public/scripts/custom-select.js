a774250f3ce3913f743731e8bcf4fbe534b953dc
Jace <buquia.jace@gmail.com>
2019-08-22 01:31:46 +0800

    Dashboard initial layout and header

---
+++ public/scripts/custom-select.js
@@ -0,0 +1,84 @@
+import Component from './component.js';
+
+class CustomSelect extends Component {
+	static selector = '.custom-select';
+
+	// Holds the currently selected option element.
+	selectedOption;
+
+	constructor(element) {
+		super(element);
+
+		this.selectedText = this.element.querySelector('.custom-select__selected');
+	}
+
+	init() {
+		this.element.addEventListener('click', e => this.handleClick(e));
+
+		this.bindOptions();
+	}
+
+	handleClick(e) {
+		e.stopPropagation();
+
+		// Toggle dropdown
+		if ( this.element.classList.contains('custom-select--expanded') ) {
+			this.closeDropdown();
+		}
+		else {
+			this.openDropdown();
+		}
+	}
+
+	handleOptionClick(e) {
+		e.stopPropagation();
+
+		// Close dropdown
+		this.closeDropdown();
+
+		// Show selected
+		this.setSelected(e.currentTarget);
+	}
+
+	bindOptions() {
+		[...this.options] = this.element.querySelectorAll('.custom-select__option');
+
+		this.options.forEach(option => {
+			option.addEventListener('click', e => this.handleOptionClick(e));
+
+			if ( option.classList.contains('custom-select__option--selected') ) {
+				this.setSelected(option);
+			}
+		});
+	}
+
+	openDropdown() {
+		this.element.classList.add('custom-select--expanded');
+	}
+
+	closeDropdown() {
+		this.element.classList.remove('custom-select--expanded');
+	}
+
+	/**
+	 * Uses a given option element to show as selected
+	 * @param {HTMLElement} option
+	 */
+	setSelected(option) {
+		const { textContent, dataset } = option;
+		this.selectedText.textContent = textContent;
+
+		if ( option !== this.selectedOption ) {
+			if ( this.selectedOption instanceof HTMLElement ) {
+				this.selectedOption.classList.remove('custom-select__option--selected');
+			}
+
+			this.selectedOption = option;
+			option.classList.add('custom-select__option--selected');
+		}
+
+		// Should emit event passing dataset.value
+	}
+}
+
+export default CustomSelect;
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,8 +1,8 @@
-import Header from './header.js';
+import CustomSelect from './custom-select.js';
 
 // Determine touch support
 if ( 'ontouchstart' in document ) {
 	document.body.classList.add('touch');
 }
 
-Header.init();
+CustomSelect.init();
